8e0ba65c9e53b7513b035b7f0c9de4e3c01bb16d
kbbarragan22 <kbbarragan22@gmail.com>
2026-05-04 19:20:08 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/', // Must match your repo name!
+  base: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : '/', // Must match your repo name!
 });
